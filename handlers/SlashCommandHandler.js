04bde40a0808dc9ba085a2aedbb626a763b9de54
PAW122 <70094237+PAW122@users.noreply.github.com>
2023-12-26 02:25:49 +0100

    +slash command handler

--- handlers/SlashCommandHandler.js
+++ handlers/SlashCommandHandler.js
@@ -42,8 +42,53 @@ async function unregisterAllCommands(client) {
         console.error('Error while unregistering commands:', error);
     }
 
-    //po usunięciu komend dodaj je na nowo tak aby usunięte komendy zostały nadpisane przez nowe
-    register_slash_commands(client)
+}
+/**
+ * 
+ * @param {string} guild guild Id
+ * @param {object} client client
+ */
+async function registerSlashCommandsForGuild(guild, client) {
+    const commandsDir = fs.readdirSync(__dirname + `\\..\\commands`);
+
+    for (const folder of commandsDir) {
+        const commandsFile = fs.readdirSync(__dirname + `\\..\\commands\\${folder}`).filter(file => file.endsWith(".js"));
+
+        for (const file of commandsFile) {
+            const filePath = __dirname + `\\..\\commands\\${folder}\\${file}`;
+            delete require.cache[require.resolve(filePath)]; // Clear the cache
+            const { command, _ } = require(filePath);
+
+            try {
+                // Register command on the specific guild
+                await guild.commands.create(command);
+                //console.log(`Command registered on guild ${guild.id}: ${command.name}`);
+            } catch (error) {
+                console.error(`Error registering command ${command.name} on guild ${guild.id}:`, error);
+            }
+        }
+    }
+
+    console.log(`All commands registered successfully on guild ${guild.id}.`);
 }
 
-module.exports = {unregisterAllCommands, register_slash_commands}
+/**
+ * 
+ * @param {string} guild guild Id
+ * @param {*} client client
+ */
+async function unregisterAllCommandsForGuild(guild, client) {
+    try {
+        const commands = await guild.commands.fetch();
+
+        for (const command of commands.values()) {
+            await guild.commands.delete(command.id);
+        }
+
+        //console.log(`All commands unregistered successfully on guild ${guild.id}.`);
+    } catch (error) {
+        console.error(`Error while unregistering commands on guild ${guild.id}:`, error);
+    }
+}
+
+module.exports = {unregisterAllCommands, register_slash_commands, registerSlashCommandsForGuild, unregisterAllCommandsForGuild}
