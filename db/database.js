1ea821e2636eb24c3a00aff40191dca57bda7640
PAW122 <70094237+PAW122@users.noreply.github.com>
2023-12-26 02:24:38 +0100

    added .add funkction
    usage:
    database.add(path, key: content)
    example:
    database.add(`${user_id}`, {[interaction_id]: content})

--- db/database.js
+++ db/database.js
@@ -69,6 +69,46 @@ class Database {
         return current;
     }
 
+    /**
+     * add
+     * Dodaje nowy wpis do bazy danych w sposób opisany przez użytkownika
+     * @param {string} path - Ścieżka do elementu w bazie danych (np. "testuser")
+     * @param {object} data - Dane do dodania np: [interaction_id]: content
+     */
+    add(path, data) {
+        const database = JSON.parse(fs.readFileSync(this.file_path, 'utf-8'));
+        const pathSegments = path.split('.');
+        const user_id = pathSegments[0];
+
+        // Sprawdza, czy użytkownik już istnieje w bazie danych
+        if (!database[user_id]) {
+            database[user_id] = {};
+        }
+
+        // Dla każdej właściwości w danych, dodaj ją do obecnej wartości (jeśli istnieje)
+        for (const key in data) {
+            if (data.hasOwnProperty(key)) {
+                // Jeżeli istnieje już wartość dla danego klucza, dodaj nową wartość do istniejącej
+                if (database[user_id][key]) {
+                    if (Array.isArray(database[user_id][key])) {
+                        // Jeżeli już jest tablica, dodaj nowy element do niej
+                        database[user_id][key].push(data[key]);
+                    } else {
+                        // Jeżeli nie jest tablicą, zamień to w tablicę i dodaj nowy element
+                        database[user_id][key] = [database[user_id][key], data[key]];
+                    }
+                } else {
+                    // Jeżeli nie istnieje jeszcze wartość dla danego klucza, po prostu przypisz ją
+                    database[user_id][key] = data[key];
+                }
+            }
+        }
+
+        // Zapisuje zmienioną bazę danych z powrotem do pliku JSON
+        fs.writeFileSync(this.file_path, JSON.stringify(database, null, 2), 'utf-8');
+        //console.log(`Nowy wpis został dodany do ${path}.`);
+    }
+
 
 }
 
