c93fa75617a51d3dfd892a250a1b3841a4690c94
PAW122 <70094237+PAW122@users.noreply.github.com>
2023-12-26 02:25:02 +0100

    +idead command

---
+++ commands/normal/idea.js
@@ -0,0 +1,31 @@
+const { SlashCommandBuilder } = require("discord.js");
+const Database = require("../../db/database");
+const database = new Database(__dirname + "\\..\\..\\db\\files\\ideas.json");
+
+const command = new SlashCommandBuilder()
+    .setName("idea")
+    .setDescription("Share your idea for new bot features")
+    .addStringOption(option => option
+        .setName("content")
+        .setDescription('describe your idea')
+        .setRequired(true)
+    )
+
+async function execute(interaction, client) {
+    const content = interaction.options.getString("content");
+    const interaction_id = interaction.id
+    const user_id = interaction.user.id
+    database.init();
+    database.add(`${user_id}`, {[interaction_id]: content})
+    await interaction.reply("saved");
+}
+
+//return message if user use /help/ping
+async function help_message(interaction, client) {
+    interaction.reply({
+        content: `Share your idea for new bot features`,
+        ephemeral: true
+    })
+}
+
+module.exports = { command, execute, help_message };
