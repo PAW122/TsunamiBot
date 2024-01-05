75d400e2f72b0d6177acc0ccaf19bec1a4d2337e
PAW122 <70094237+PAW122@users.noreply.github.com>
2024-01-06 00:37:29 +0100

    info update

--- commands/normal/info.js
+++ commands/normal/info.js
@@ -5,9 +5,9 @@ const command = new SlashCommandBuilder()
     .setDescription("Informations about project");
 
 async function execute(interaction) {
-    await interaction.reply(`Hello, I'm starting to create a Tsunami Cat Bot discord bot project. More bot features will be added over time, bot development will accelerate if I get any response from tsu <3.
+    await interaction.reply(`Hello, I'm starting to create a Tsunami Cat Bot discord bot project. More bot features will be added over time, bot development will accelerate if I get any response from tsu whether the bot has a chance to be added to this server <3.
     
-    The plans include economics, statistics, a bot website and much more. Soon it will be possible to add your suggestions for new bot functions`);
+    The plans include economics, statistics, a bot website and much more. Soon it will be possible to add your suggestions for new bot functions on the bot website . At this point, you can add suggestions using **/idea**`);
 }
 
 //return message if user use /help/ping
