a0cf0b3702388ce449147ceb8d2996679acff5d3
PAW122 <70094237+PAW122@users.noreply.github.com>
2024-01-08 01:08:54 +0100

    save data functions

---
+++ handlers/_save_basics_data.js
@@ -0,0 +1,37 @@
+const Database = require("../db/database");
+const database = new Database(__dirname + "\\..\\db\\files\\servers.json");
+
+const ConsoleLogger = require("../handlers/console")
+const logger = ConsoleLogger.getInstance();
+/**
+ * return basics informations about server (from db)
+ * @param {*} server_id 
+ */
+function return_basics(server_id) {
+    
+}
+
+/**
+ * check is in db data about server exist
+ * @param {*} server_id 
+ */
+function check_basics(server_id) {
+
+}
+
+/**
+ * save data in db about server
+ * @param {*} server_id 
+ */
+function save_basics(server_id) {
+    logger.log(`save basics ${server_id}`)
+    const { client } = require("../main");
+    const guild = client.guilds.cache.get(server_id);
+    const data = {
+        name: guild.name,
+        owner: guild.ownerId
+    }
+    database.push(`${server_id}`, data)
+}
+
+module.exports = {save_basics, check_basics, return_basics}
--- web/routes/server_settings.js
+++ web/routes/server_settings.js
@@ -3,12 +3,16 @@ const database = new Database(__dirname + "\\..\\..\\db\\files\\servers.json");
 
 const ConsoleLogger = require("../../handlers/console")
 const logger = ConsoleLogger.getInstance();
+
+const { save_basics } = require("../../handlers/_save_basics_data")
+
 /**
  * 
  * @param {*} type - db variable name
  * @param {*} value - variable balue
  */
 function save_server_settings(type, value, server_id) {
+    save_basics(server_id)
     if(value === "true") value = true;
     else if(value === "false") value = false;
 
@@ -23,10 +27,43 @@ function save_server_settings(type, value, server_id) {
  * @param {*} server_id 
  */
 function load_server_settings(type, server_id) {
-    console.log(type, server_id)
     database.init()
     const data = database.read(`${server_id}.${type}`)
     return data;
 }
 
-module.exports = { save_server_settings, load_server_settings }
+/**
+ * 
+ * @param {*} name 
+ * @param {*} server_id 
+ */
+function save_bot_username(name, server_id) {
+    
+    var { client } = require("../../main")
+    // Pobierz obiekt serwera (Guild) na podstawie ID
+    const server = client.guilds.cache.get(server_id);
+
+    if (server) {
+        // Pobierz obiekt bota na danym serwerze
+        const botMember = server.members.cache.get(client.user.id);
+
+        if (botMember) {
+            // Ustaw nick bota na serwerze
+            botMember.setNickname(name)
+                .then(() => {
+                    logger.extra(`Successfully set bot's nickname to "${name}" on server ${server.name}`);
+
+                    save_server_settings("bot_nickname",name ,server_id)
+                })
+                .catch(error => {
+                    logger.error('Error setting bot nickname:', error);
+                });
+        } else {
+            logger.error('Bot is not a member of the specified server');
+        }
+    } else {
+        logger.error('Server not found');
+    }
+}
+
+module.exports = { save_server_settings, load_server_settings, save_bot_username }
