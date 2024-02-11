d791f2539b9a6bc9c0af5f3955838cc35f561952
filipkruza <74458014+filipkruza@users.noreply.github.com>
2024-02-11 20:31:47 +0100

    working server list

--- web2/scripts/main.js
+++ web2/scripts/main.js
@@ -1,4 +1,5 @@
 document.onload = load()
+document.logout = logout;
 
 var urlParams;
 
@@ -18,11 +19,42 @@ function load() {
             .catch(err => { throw err }
             );
     } else {
-        console.log("token not found")
+        console.log("token not found");
     }
 }
 
-function login(serverList) {
-    console.log("logged in")
-    console.log("serverlist: ", serverList)
+function login(serverListResponse) {
+    let userinfo = serverListResponse["user"]
+    document.querySelectorAll(".profile-logout").forEach(function (el) {
+        el.classList.add("d-none");
+    })
+    document.querySelectorAll(".profile-login").forEach(function (el) {
+        el.classList.remove("d-none");
+    })
+    document.getElementById("username-text").textContent = `@` + userinfo["username"];
+    document.getElementById("profile-picture").src = `https://cdn.discordapp.com/avatars/${userinfo["id"]}/${userinfo["avatar"]}.jpg`;
+    let servers = serverListResponse["servers"];
+    let serversContainer = document.getElementById("servers");
+    servers.forEach(function (server) {
+        let parentDiv = document.createElement("div");
+        parentDiv.classList.add("d-flex", "p-3", "bg-body-secondary", "rounded");
+        let icon = document.createElement("img");
+        icon.src = `https://cdn.discordapp.com/icons/${server["id"]}/${server["icon"]}.png`;
+        icon.height = "25";
+        icon.classList.add("me-2", "rounded")
+        let name = document.createElement("div");
+        name.textContent = server["name"];
+        parentDiv.append(icon, name);
+        serversContainer.appendChild(parentDiv);
+    })
+}
+
+function logout() {
+    document.getElementById("servers").innerHTML = "";
+    document.querySelectorAll(".profile-logout").forEach(function (el) {
+        el.classList.remove("d-none");
+    })
+    document.querySelectorAll(".profile-login").forEach(function (el) {
+        el.classList.add("d-none");
+    })
 }
