9204d534e699a875fbcf97d6b560b60e9950691f
filipkruza <74458014+filipkruza@users.noreply.github.com>
2024-02-11 19:11:56 +0100

    added panel SPA

--- web2/scripts/main.js
+++ web2/scripts/main.js
@@ -1,1 +1,27 @@
-alert("working")
+document.onload = load()
+
+var urlParams;
+
+function load() {
+    urlParams = new URLSearchParams(window.location.hash);
+    let token = urlParams.get("access_token");
+    let token_type = urlParams.get("#token_type");
+    if (token && token_type) {
+        console.log(`token: ${token}`);
+        fetch(`http://localhost:3000/load/server-list/${token_type}/${token}`)
+            .then(res => res.json())
+            .then(out => {
+                console.log('Downloaded JSON: ', out)
+                login(out)
+            })
+            .catch(err => { throw err }
+            );
+    } else {
+        console.log("token not found")
+    }
+}
+
+function login(serverList) {
+    console.log("logged in")
+    console.log("serverlist: ", serverList)
+}
