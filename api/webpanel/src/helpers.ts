7a3cfaae4613b5dd9723de880f82686b8920a4a7
filipkruza <74458014+filipkruza@users.noreply.github.com>
2024-02-25 14:43:42 +0100

    async UI updates

--- api/webpanel/src/helpers.ts
+++ api/webpanel/src/helpers.ts
@@ -1,10 +1,11 @@
-export function doFetch(url: string, callback: Function) {
-    console.debug(`fetching ${url}`)
-    fetch(url)
-        .then(res => res.json())
-        .then(out => {
-            console.debug(`Downloaded URL: `, out)
-            callback(out)
-        })
-        .catch(err => { throw err })
+export function navbarStyle() {
+    let el = document.querySelector(".navbar.sticky-top");
+    let observer = new IntersectionObserver(
+        ([e]) => e.target.classList.toggle("is-pinned", e.intersectionRatio < 1),
+        { threshold: [1] }
+    );
+    if (el) {
+        console.debug("applied navbar observer");
+        observer.observe(el);
+    }
 }
--- api/webpanel/src/main.ts
+++ api/webpanel/src/main.ts
@@ -1,13 +1,15 @@
 // this file is a template, which will be replaced
 import { drawServers } from "./ServerList.js"
 import * as config from "./config.js"
-import { doFetch } from "./helpers.js"
+import { navbarStyle } from "./helpers.js"
 import { auth } from "./login.js"
+import { genSettings } from "./settings.js"
 
 window.onload = initial
 const loginManager = new auth()
 
 function initial() {
+    navbarStyle();
     if (String(config.MainURL) === "http://localhost:3000") {
         document.getElementById("test-banner")?.classList.remove("d-none")
     }
@@ -21,17 +23,18 @@ function initial() {
         document.querySelectorAll(".profile-login").forEach(function (el) {
             el.classList.remove("d-none");
         })
-        doFetch(`${config.MainURL}/load/server-list/${loginManager.token.token_type}/${loginManager.token.token}`, login)
+        login();
+        // doFetch(`${config.MainURL}/load/server-list/${loginManager.token.token_type}/${loginManager.token.token}`, login)
     }
     window["lm"] = loginManager
 }
 
-
-function login(serverListResponse) {
-    let userinfo = serverListResponse["user"];
-    document.getElementById("username-text")!.textContent = `@${userinfo["username"]}`
-    document.getElementById("profile-picture")?.setAttribute("src", `https://cdn.discordapp.com/avatars/${userinfo["id"]}/${userinfo["avatar"]}.jpg`)
-    let servers = serverListResponse["servers"];
+async function login() {
+    let response = await fetch(`${config.MainURL}/load/server-list/${loginManager.token.token_type}/${loginManager.token.token}`);
+    let json = await response.json();
+    document.getElementById("username-text")!.textContent = `@${json.user.username}`
+    document.getElementById("profile-picture")?.setAttribute("src", `https://cdn.discordapp.com/avatars/${json.user.id}/${json.user.avatar}.jpg`)
+    let servers = json.servers;
     drawServers(servers, handleServerClick);
     document.querySelector("#logout-button")?.addEventListener("click", function () {
         loginManager.dispose();
@@ -39,142 +42,113 @@ function login(serverListResponse) {
     });
 }
 
-function handleServerClick(clickedServerId: string) {
-    console.log(`Button with value ${clickedServerId} clicked`);
-    let welcomeChannelSelect: HTMLSelectElement;
-    let welcomeMessageCheck: HTMLInputElement;
-    let autoroleSelect: HTMLSelectElement;
-    let autoroleCheck: HTMLInputElement;
-    function genSettings() {
-        let settings_parent = document.getElementById("settings_container") as HTMLDivElement
-        settings_parent.innerHTML = ""
-        let aa = document.createElement("div");
-        aa.classList.add("d-flex", "flex-column", "gap-2")
-        let ab = document.createElement("div");
-        ab.classList.add("h3");
-        ab.textContent = "Welcome messages";
-        welcomeChannelSelect = document.createElement("select") as HTMLSelectElement;
-        welcomeChannelSelect.classList.add("form-select");
-        welcomeMessageCheck = document.createElement("input") as HTMLInputElement;
-        welcomeMessageCheck.type = "checkbox";
-        welcomeMessageCheck.classList.add("form-check-input");
+async function handleServerClick(clickedServerId: string) {
+    console.debug(`Loading server with ID ${clickedServerId}`);
+    let settings_parent = document.getElementById("settings_container") as HTMLDivElement;
+    settings_parent!.innerHTML = `<div class="d-flex p-3 bg-body-secondary rounded align-items-center placeholder-glow">
+    <span class="placeholder col-1"></span><span class="col-1"></span><span class="placeholder col-10"></span></div>
+    <div class="p-3 bg-body-secondary rounded align-items-center placeholder-glow"><span class="placeholder col-7">
+    </span><span class="placeholder col-4"></span><span class="placeholder col-4"></span><span class="placeholder col-6">
+    </span><span class="placeholder col-8"></span></div>`;
 
-        let ba = document.createElement("div");
-        ba.classList.add("d-flex", "flex-column", "gap-2")
-        let bb = document.createElement("div");
-        bb.classList.add("h3");
-        bb.textContent = "Autorole";
-        autoroleSelect = document.createElement("select") as HTMLSelectElement;
-        autoroleSelect.classList.add("form-select");
-        autoroleCheck = document.createElement("input") as HTMLInputElement;
-        autoroleCheck.type = "checkbox";
-        autoroleCheck.classList.add("form-check-input");
-
-        aa.appendChild(ab);
-        aa.appendChild(welcomeChannelSelect);
-        aa.appendChild(welcomeMessageCheck);
-
-        ba.appendChild(bb);
-        ba.appendChild(autoroleSelect);
-        ba.appendChild(autoroleCheck);
-
-        settings_parent.appendChild(aa);
-        settings_parent.appendChild(ba);
-    }
-    genSettings()
-
-    const body = {
-        tokenType: loginManager.token.token_type,
-        token: loginManager.token.token,
-        server_id: clickedServerId
-    };
-    
-    console.log("loaded")
-    console.log(body)
-    interface MyResponse {
-        welcome_message_content: string;
-        welcome_message_enable: boolean;
-        welcome_message_channel: any;
-        server_channels_list: any;
-        autorole_enable: boolean;
-        autorole_role: any;
-        server_roles_list: any;
-    }
-
-    fetch(`${config.MainURL}/full_load/content`, {
+    let response = await fetch(`${config.MainURL}/full_load/content`, {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json'
         },
-        body: JSON.stringify(body)
-    }).then((response: Response) => {
-        return response.json(); // Parsowanie odpowiedzi do JSON
-    })
-        .then((data: MyResponse) => {
-            // Tutaj możesz uzyskać dostęp do welcome_message_content
-            //const welcomeMessageContent = data.welcome_message_content;
-            const welcome_message_enable = data.welcome_message_enable;
-            const welcome_message_channel = data.welcome_message_channel;
-            const server_channels_list = data.server_channels_list;
-            const autorole_enable = data.autorole_enable;
-            const autorole_role = data.autorole_role;
-            const server_roles_list = data.server_roles_list;
-
-
-            welcomeMessageCheck.checked = welcome_message_enable
-            autoroleCheck.checked = autorole_enable
-            server_channels_list?.forEach((channel, _index) => {
-                const option = document.createElement('option');
-                option.value = channel.id;
-                option.text = channel.name.length > 32 ? channel.name.substring(0, 29) + "..." : channel.name;
-                welcomeChannelSelect.options.add(option)
-
-                // Ustaw opcję jako wybraną, jeśli jej id zgadza się z id zwróconym z serwera
-                if (channel.id === welcome_message_channel.id) {
-                    option.selected = true;
-                }
-            });
-            if(server_roles_list) {
-                server_roles_list.forEach((role, _index) => {
-                    const option = document.createElement('option');
-                    option.value = role.id;
-                    option.text = role.name.length > 32 ? role.name.substring(0, 29) + "..." : role.name;
-                    autoroleSelect.options.add(option)
-                    // Ustaw opcję jako wybraną, jeśli jej id zgadza się z id zwróconym z serwera
-                    if (role.id === autorole_role.id) {
-                        option.selected = true;
-                    }
-                });
-            }
-
-        })
-        .catch((error: Error) => {
-            console.log(error)
-        });
-
-    // Save Welcome Messages status
-    welcomeMessageCheck!.addEventListener("change", function () {
-        doFetch(`${config.MainURL}/save/welcome_messages_status/${loginManager.token.token_type}/${loginManager.token.token}/${clickedServerId}/${welcomeMessageCheck.checked}`, (res) => {
-            console.log(`Welcome message set to ${welcomeMessageCheck.checked}. Response: `, res)
+        body: JSON.stringify({
+            tokenType: loginManager.token.token_type,
+            token: loginManager.token.token,
+            server_id: clickedServerId
         })
     })
-    // Save Auto Role status
-    autoroleCheck!.addEventListener("change", function () {
-        doFetch(`${config.MainURL}/save/auto_role_status/${loginManager.token.token_type}/${loginManager.token.token}/${clickedServerId}/${autoroleCheck.checked}`, (res) => {
-            console.log(`Autorole set to ${autoroleCheck.checked}. Response: `, res)
-        })
-    })
-    // save welcome channels
-    welcomeChannelSelect!.addEventListener("change", function () {
-        doFetch(`${config.MainURL}/save/welcome_messages_channel/${loginManager.token.token_type}/${loginManager.token.token}/${clickedServerId}/${welcomeChannelSelect.value}`, (res) => {
-            console.log(`Welcome Message Channel set to: ${welcomeChannelSelect.value}. Response: `, res)
-        })
+    let body = await response.json();
+    if (!response.ok) {
+        throw new Error("Response error");
+    }
+    settings_parent!.innerHTML = "";
+
+    let autorole = genSettings(settings_parent, "Autorole", true);
+    let welcome_channel = genSettings(settings_parent, "Welcome Channel", true);
+
+    // reading autorole switches
+    if (body.autorole_enable === false) {
+        autorole.checkbox!.checked = false;
+        autorole.select.setAttribute("disabled", "true");
+    } else if (body.autorole_enable === true) {
+        autorole.checkbox!.checked = true;
+    } else {
+        throw new Error("Corrupted autorole response data");
+    }
+    body.server_roles_list?.forEach((role, _index) => {
+        const option = document.createElement('option');
+        option.value = role.id;
+        option.text = role.name.length > 32 ? role.name.substring(0, 29) + "..." : role.name;
+        autorole.select.options.add(option)
+        // Ustaw opcję jako wybraną, jeśli jej id zgadza się z id zwróconym z serwera
+        if (role.id === body.autorole_role.id) {
+            option.selected = true;
+        }
     });
-    // save autorole roles
-    autoroleSelect!.addEventListener("change", function () {
-        doFetch(`${config.MainURL}/save/auto_role_id/${loginManager.token.token_type}/${loginManager.token.token}/${clickedServerId}/${this.value}`, (res) => {
-            console.log(`Autorole Selected: ${autoroleSelect.value}. Response: `, res)
-        })
+
+    // reading welcome message switches
+    if (body.welcome_message_enable === false) {
+        welcome_channel.checkbox!.checked = false;
+        welcome_channel.select.setAttribute("disabled", "true");
+    } else if (body.welcome_message_enable === true) {
+        welcome_channel.checkbox!.checked = true;
+    } else {
+        throw new Error("Corrupted welcome message response data");
+    }
+    body.server_channels_list?.forEach((channel, _index) => {
+        const option = document.createElement('option');
+        option.value = channel.id;
+        option.text = channel.name.length > 32 ? channel.name.substring(0, 29) + "..." : channel.name;
+        welcome_channel.select.options.add(option);
+        // Ustaw opcję jako wybraną, jeśli jej id zgadza się z id zwróconym z serwera
+        if (channel.id === body.welcome_message_channel.id) {
+            option.selected = true;
+        }
+    });
+
+    // saving autorole switches
+    autorole.checkbox!.addEventListener("change", async function () {
+        let response = await fetch(`${config.MainURL}/save/auto_role_status/${loginManager.token.token_type}/${loginManager.token.token}/${clickedServerId}/${this.checked}`);
+        if (response.ok) {
+            console.log(`Autorole set to ${autorole.checkbox?.checked}. Response: ${response.status}`);
+        } else {
+            console.warn(`Error setting autorole to ${autorole.checkbox?.checked}. Refreshing`);
+            handleServerClick(clickedServerId);
+        }
+    })
+    autorole.select!.addEventListener("change", async function () {
+        let response = await fetch(`${config.MainURL}/save/auto_role_id/${loginManager.token.token_type}/${loginManager.token.token}/${clickedServerId}/${this.value}`);
+        if (response.ok) {
+            console.log(`Autorole role set to ${this.value}. Response: ${response.status}`);
+        } else {
+            console.warn(`Error setting autorole role to ${this.value}. Refreshing`);
+            handleServerClick(clickedServerId);
+        }
+    })
+
+    // saving welcome message switches
+    welcome_channel.checkbox!.addEventListener("change", async function () {
+        let response = await fetch(`${config.MainURL}/save/welcome_messages_status/${loginManager.token.token_type}/${loginManager.token.token}/${clickedServerId}/${this.checked}`);
+        if (response.ok) {
+            console.log(`Welcome message set to ${this.checked}. Response: ${response.status}`);
+        } else {
+            console.warn(`Error setting autorole to ${this.checked}. Refreshing`);
+            handleServerClick(clickedServerId);
+        }
+    })
+    welcome_channel.select!.addEventListener("change", async function () {
+        let response = await fetch(`${config.MainURL}/save/welcome_messages_channel/${loginManager.token.token_type}/${loginManager.token.token}/${clickedServerId}/${this.value}`);
+        if (response.ok) {
+            console.log(`Welcome message channel set to ${this.value}. Response: ${response.status}`);
+        } else {
+            console.warn(`Error setting welcome message channel to ${this.value}. Refreshing`);
+            handleServerClick(clickedServerId);
+        }
     })
 
 }
