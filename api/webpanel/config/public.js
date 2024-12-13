b2d135fc6c10c84beebf2c382986cf5a5c1b4eb4
PAW122 <pawelklin10@gmail.com>
2024-12-13 16:21:40 -0500

    server

--- api/webpanel/config/public.js
+++ api/webpanel/config/public.js
@@ -1,3 +1,3 @@
 export const BotInviteURL = "bot invite URL"
-export const AuthURL = "https://discord.com/api/oauth2/authorize?client_id=928399458570502155&response_type=token&redirect_uri=https%3A%2F%2Fsenko.pagekite.me%2F&scope=identify+guilds+email"
-export const MainURL = "https://senko.pagekite.me"
+export const AuthURL = "https://discord.com/api/oauth2/authorize?client_id=1234909673702817903&response_type=token&redirect_uri=https%3A%2F%2Fgreen.tsunamistudio.net%2F&scope=identify+guilds+email"
+export const MainURL = "https://green.tsunamistudio.net"
