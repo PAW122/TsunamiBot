6e35a2086f2d2b167d4297f277220f5a4a273dbe
PAW122 <70094237+PAW122@users.noreply.github.com>
2025-01-07 03:30:54 +0100

    fix bug - invalid username

--- commands/normal/lvl.js
+++ commands/normal/lvl.js
@@ -50,29 +50,29 @@ async function execute(interaction, client) {
 
     } else {
         //show marked user data
-        const user = user_option.id
-        const guild = interaction.guild.id
-        const user_data = await db.read(`${guild}.users_lvls.${user}`)
-        if (!user_data) {
+        const secound_user = user_option.id
+        const secound_guild = interaction.guild.id
+        const secound_user_data = await db.read(`${secound_guild}.users_lvls.${secound_user}`)
+        if (!secound_user_data) {
             const embed = new EmbedBuilder()
                 .setColor('#0099ff')
-                .setTitle(`${user_option.username}`)
+                .setTitle(`${user_option.username || user_option.globalName}`)
                 .setDescription(`**Level:** 0\n**XP:** 0\n**Messages:** 0`)
                 .setTimestamp();
 
             return await interaction.reply({ embeds: [embed] })
         }
-        const xp = user_data.xp
-        const messages = user_data.messages
-        const lvl_calculator = calculate_lvl(xp)
-        const level = lvl_calculator.level
-        const remainingXP = lvl_calculator.remainingXP
-        const NextLevel = lvl_calculator.NextLevel
+        const secound_xp = secound_user_data.xp
+        const secound_messages = secound_user_data.messages
+        const secound_lvl_calculator = calculate_lvl(secound_xp)
+        const secound_level = secound_lvl_calculator.level
+        const secound_remainingXP = secound_lvl_calculator.remainingXP
+        const secound_NextLevel = secound_lvl_calculator.NextLevel
 
         const embed = new EmbedBuilder()
             .setColor('#0099ff')
-            .setTitle(`${interaction.user.username}`)
-            .setDescription(`**Level:** ${level}\n**XP:** ${remainingXP}/${NextLevel}\n**Messages:** ${messages}`)
+            .setTitle(`${user_option.username || user_option.globalName}`)
+            .setDescription(`**Level:** ${secound_level}\n**XP:** ${secound_remainingXP}/${secound_NextLevel}\n**Messages:** ${secound_messages}`)
             .setTimestamp();
 
         await interaction.reply({ embeds: [embed] })
